47be1baf2eaead0709fa4a29da349b50ca8ff6e2
miraj <miraj.luminoguru@gmail.com>
2021-12-02 17:55:31 +0530

    add download

--- src/components/inc/pagination.js
+++ src/components/inc/pagination.js
@@ -3,11 +3,13 @@ import {Pagination,PaginationItem} from '@mui/material';
 import Stack from '@mui/material/Stack';
 const Pagina = () => {
     return (
-        <Stack spacing={2}>
-            <Pagination count={5} color="primary" >
-                <PaginationItem page={1}/>
-            </Pagination>
-        </Stack>
+        <div className="pagination-wrapper">
+            <Stack spacing={2}>
+                <Pagination count={5} color="primary" >
+                    <PaginationItem page={1}/>
+                </Pagination>
+            </Stack>
+        </div> 
     )
 }
 
